99eb54272054985002caa09a002e5afe2aa5562b
unknown <maygdoria@gmail.com>
2026-03-15 16:46:32 -0300

    Criação do service de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -1,43 +1,22 @@
-package com.fiap.ec.backend_consultas.model;
-import jakarta.persistence.*;
-import java.time.LocalDate;
-@Entity
-@Table(name = "pacientes")
-public class Paciente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false)
-    private String nome;
-    @Column(nullable = false, unique = true)
-    private String cpf;
-    @Column(nullable = false)
-    private String email;
-    private String telefone;
-    private LocalDate dataNascimento;
-    private Boolean ativo;
-    public Paciente() {
+package com.fiap.ec.backend_consultas.service;
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class PacienteService {
+    private final PacienteRepository repository;
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
     }
-    public Paciente(String nome, String cpf, String email,
-                    String telefone, LocalDate dataNascimento, Boolean ativo) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.telefone = telefone;
-        this.dataNascimento = dataNascimento;
-        this.ativo = ativo;
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
     }
-    public Long getId() { return id; }
-    public String getNome() { return nome; }
-    public String getCpf() { return cpf; }
-    public String getEmail() { return email; }
-    public String getTelefone() { return telefone; }
-    public LocalDate getDataNascimento() { return dataNascimento; }
-    public Boolean getAtivo() { return ativo; }
-    public void setNome(String nome) { this.nome = nome; }
-    public void setCpf(String cpf) { this.cpf = cpf; }
-    public void setEmail(String email) { this.email = email; }
-    public void setTelefone(String telefone) { this.telefone = telefone; }
-    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
-    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
+    public Paciente buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente não encontrado"));
+    }
 }
